86609d85d3a538e5afecac66fe71acda55746d74
forrest321 <forrest321@gmail.com>
2020-10-13 23:00:15 -0500

    getting sleepy

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/forrest321/mapoverlay
 go 1.14
 
 require (
-	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/disintegration/imaging v1.6.2
+	github.com/forrest321/loc2map v0.0.0-20201013030023-edfd09688370
 	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
 )
--- mapoverlay.go
+++ mapoverlay.go
@@ -3,10 +3,14 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/disintegration/imaging"
 	"github.com/rwcarlsen/goexif/exif"
+	"image"
+	"image/color"
+	"io/ioutil"
 	"log"
 	"os"
-	""
+	"github.com/forrest321/loc2map"
 )
 
 type loc struct {
@@ -16,10 +20,11 @@ type loc struct {
 
 
 // TODO: 1) Get Coords 					[X]
-// TODO: 2) Get Map    					[ ] //kinda done, just call loc2map
-// TODO: 3) Superimpose Map over image 	[ ]
-// TODO:  a) pick package				[ ]
-// TODO:  b) use package 				[ ]
+// TODO: 2) Get Map    					[X] //kinda done, just call loc2map
+// TODO: 3) Copy Original				[X]
+// TODO: 4) Superimpose Map over image 	[X]
+// TODO: 5) Check work from 3 & 4		[ ]
+
 func main() {
 	goodFile := "DJI_0289.JPG"
 	badFile := "doit.jpg"
@@ -30,6 +35,14 @@ func main() {
 }
 
 func buildCompositeImage(fileName string) error {
+	sourceFileStat, err := os.Stat(fileName)
+	if err != nil {
+		return err
+	}
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", fileName)
+	}
+
 	loc, err := getCoords(fileName)
 	if err != nil {
 		if loc == nil {
@@ -37,15 +50,40 @@ func buildCompositeImage(fileName string) error {
 			log.Fatal(err)
 		}
 		// location is not nil, might be recoverable
+		// if we got here, there was prob a failure in the file.Close() which can prob recover
 		log.Print(err)
 	}
 
-	// the following TODOs assume use of https://github.com/disintegration/imaging
-	//TODO: 1: call github.com/forrest321/loc2map method with coords to get map
-	//TODO: 2: create new file (do not modify incoming, original file) with original's dimensions
-	//TODO: 3: paste files in, creating new file
+	src, err := imaging.Open(fileName)
+	if err != nil {
+		return err
+	}
 
-	return nil
+	// the following TODOs assume use of https://github.com/disintegration/imaging
+	//TODO: [X] 1: call github.com/forrest321/loc2map method with coords to get map
+	mapImage, err := loc2map.Loc2ByteArrayOfMapImage(loc.lat, loc.lng)
+	if err != nil {
+		return err
+	}
+
+	// Write the image data to file
+	if err := ioutil.WriteFile("map-tmp.png", mapImage, 0644); err != nil {
+		log.Fatal(err)
+	}
+
+	//TODO: [X] 2: create new file (do not modify incoming, original file) with original's dimensions
+
+	mapImg, err := imaging.Open("map-tmp.png")
+	if err != nil {
+		return err
+	}
+	mapImg = imaging.Resize(mapImg, src.Bounds().Max.X / 2, src.Bounds().Max.Y / 2, imaging.Lanczos)
+
+	//TODO: [X] 3: create new image, paste files in
+	newImg := imaging.New(src.Bounds().Max.X, src.Bounds().Max.Y, color.NRGBA{})
+	newImg = imaging.Paste(newImg, src, image.Pt(0,0))
+	newImg = imaging.Paste(newImg, mapImg, image.Pt(0,0))
+	return imaging.Save(newImg, "output.png", imaging.PNGCompressionLevel(1))
 }
 
 func getCoords(fileName string) (*loc, error) {
@@ -61,9 +99,11 @@ func getCoords(fileName string) (*loc, error) {
 	if err != nil {
 		return nil, err
 	}
+	_ = f.Close() //close explicitly instead of waiting for defer. could also log possible error
+
 	lat, long, err := x.LatLong()
 	if err != nil {
 		return nil, err
 	}
-	return &loc{lat: lat,lng: long}, f.Close()
+	return &loc{lat: lat,lng: long}, nil
 }
